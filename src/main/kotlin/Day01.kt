455f5adc07381c395a4df76889f254c56f787452
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-01 12:30:31 +0100

    day1 solution

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,18 +1,38 @@
+import kotlin.math.abs
+
 fun main() {
+
+    fun parseLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        return input.map {
+            val parts = it.split("\\s+".toRegex())
+            parts[0].toInt() to parts[1].toInt()
+        }
+            .toList()
+            .unzip()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (first, second) = parseLists(input)
+            .let {
+                it.first.sorted() to it.second.sorted()
+            }
+        return first.zip(second)
+            .sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (first, second) = parseLists(input)
+        return first.sumOf { left ->
+            left * second.count { right ->
+                right == left
+            }
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
