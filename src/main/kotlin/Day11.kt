d2cfce03cb57060a50ebe941eea262ff78fea83f
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-11 08:12:18 +0100

    day11 part 2

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -12,13 +12,12 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         return input.first().toLongList()
-            .flatMap { it.splitStone(75) }
-            .count().toLong()
+            .sumOf { it.countSplitStone(75) }
     }
 
     val testInput = readInput("Day11_test")
     part1(testInput).testAndPrint(55312L)
-    part2(testInput).testAndPrint(1L)
+    part2(testInput).testAndPrint()
 
     val input = readInput("Day11")
     part1(input).testAndPrint()
@@ -44,5 +43,48 @@ private fun Long.splitStones(stepsMissing: Int): List<Long> {
         return listOf(firstHalf, secondHalf).flatMap { it.splitStones(stepsMissing - 1) }
     }
     return listOf(this * 2024).flatMap { it.splitStones(stepsMissing - 1) }
+
+}
+
+val cache = mutableMapOf<Pair<Long, Int>, Triple<Int, List<Long>, Long>>()
+
+private fun Long.countSplitStone(steps: Int): Long {
+    return countSplitStones(steps)
+}
+
+private fun Long.countSplitStones(stepsMissing: Int): Long {
+    if (cache.contains(Pair(this, stepsMissing))) {
+        val (cachedSteps, cachedList, cachedResult) = cache[Pair(this, stepsMissing)]!!
+        if (cachedSteps == stepsMissing) {
+            return cachedResult
+        }
+        if (cachedSteps < stepsMissing) {
+            return cachedList.sumOf { it.countSplitStones(stepsMissing - cachedSteps) }
+        }
+    }
+    if (stepsMissing == 0) {
+        return 1
+    }
+    if (this == 0L) {
+        val list = listOf(1L)
+        val result = list.sumOf { it.countSplitStones(stepsMissing - 1) }
+        cache[Pair(0, stepsMissing)] = Triple(stepsMissing, list, result)
+        return result
+    }
+    if (this.toString().length % 2 == 0) {
+        val string = this.toString()
+        val half = string.length / 2
+        val firstHalf = string.substring(0, half).toLong(10)
+        val secondHalf = string.substring(half).toLong(10)
+        val list = listOf(firstHalf, secondHalf)
+        val result = list.sumOf { it.countSplitStones(stepsMissing - 1) }
+        cache[Pair(this, stepsMissing)] = Triple(stepsMissing, list, result)
+        return result
+    }
+
+    val list = listOf(this * 2024)
+    val result = list.sumOf { it.countSplitStones(stepsMissing - 1) }
+    cache[Pair(this, stepsMissing)] = Triple(stepsMissing, list, result)
+    return result
 
 }
