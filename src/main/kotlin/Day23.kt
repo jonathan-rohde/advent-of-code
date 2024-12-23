5646a701588f12ec19fcd18b88d6c696b6928e03
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-23 11:23:45 +0100

    day23 part1

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -3,7 +3,7 @@ import utils.testAndPrint
 
 fun main() {
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        return input.countPairsStartingWith("t")
     }
 
     fun part2(input: List<String>): Long {
@@ -11,10 +11,40 @@ fun main() {
     }
 
     val testInput = readInput("Day23_test")
-    part1(testInput).testAndPrint(1)
-    part2(testInput).testAndPrint(1)
+    part1(testInput).testAndPrint(7L)
+    part2(testInput).testAndPrint()
 
     val input = readInput("Day23")
     part1(input).testAndPrint()
     part2(input).testAndPrint()
 }
+
+private fun List<String>.countPairsStartingWith(t: String): Long {
+    val pairs = parse()
+    val network: MutableMap<String, List<String>> = mutableMapOf<String, List<String>>().withDefault { emptyList() }
+    pairs.forEach { (a, b) ->
+        network[a] = network.getValue(a) + b
+        network[b] = network.getValue(b) + a
+    }
+
+    val result = mutableSetOf<Triple<String, String, String>>()
+
+    network.entries.forEach { (a, listA) ->
+        network.entries.forEach { (b, listB) ->
+            network.entries.forEach { (c, listC) ->
+                if (listA.contains(b) && listB.contains(c) && listC.contains(a)) {
+                    val (x, y, z) = listOf(a, b, c).sorted().distinct()
+                    result.add(Triple(x, y, z))
+                }
+
+            }
+        }
+    }
+
+    return result.count { (a, b, c) ->
+        listOf(a, b, c).any { it.startsWith(t) }
+    }.toLong()
+}
+
+private fun String.parse(): Pair<String, String> = split("-").let { it[0] to it[1] }
+private fun List<String>.parse(): List<Pair<String, String>> = map { it.parse() }
