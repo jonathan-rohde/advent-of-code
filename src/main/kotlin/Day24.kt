a004d88b0f9a37eeeabac3f89c2679e1fa6efb85
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-24 21:03:25 +0100

    day24 part 2

--- src/main/kotlin/Day24.kt
+++ src/main/kotlin/Day24.kt
@@ -7,13 +7,14 @@ fun main() {
         return t.solve()
     }
 
-    fun part2(input: List<String>): Long {
-        return input.size.toLong()
+    fun part2(input: List<String>): String {
+        val system = input.parseProblem()
+        val rules = system.second.toList()
+        return rules.wrongWires().sorted().joinToString(",")
     }
 
     val testInput = readInput("Day24_test")
     part1(testInput).testAndPrint(4L)
-    part2(testInput).testAndPrint()
 
     val input = readInput("Day24")
     part1(input).testAndPrint()
@@ -25,19 +26,19 @@ private fun Pair<List<Gate>, List<Rule>>.solve(): Long {
     val gates = first.associate { it.first to it.second }.toMutableMap()
 
     while (rules.isNotEmpty()) {
-        val nextRule = rules.first {
+        val nextRule = rules.firstOrNull {
             it.first.first in gates && it.first.third in gates
-        }
+        } ?: return -1L
         rules.remove(nextRule)
         val (a, op, b) = nextRule.first
-        val c = nextRule.second
+        val output = nextRule.second
         val value = when (op) {
             "AND" -> gates[a]!! and gates[b]!!
             "OR" -> gates[a]!! or gates[b]!!
             "XOR" -> gates[a]!! xor gates[b]!!
             else -> error("Invalid operation: $op")
         }
-        gates[c] = value
+        gates[output] = value
     }
 
     val result = gates.entries.filter { it.key.startsWith("z") }
@@ -68,17 +69,80 @@ private fun String.parseState(): Gate {
 }
 
 private fun String.parseRule(): Rule {
-    val match = "([a-z]+\\d*) (AND|OR|XOR) ([a-z]+\\d*) -> ([a-z]+\\d*)".toRegex().find(this) ?: error("Invalid rule: $this")
+    val match =
+        "([a-z]+\\d*) (AND|OR|XOR) ([a-z]+\\d*) -> ([a-z]+\\d*)".toRegex().find(this) ?: error("Invalid rule: $this")
     val (a, op, b, c) = match.destructured
     return Triple(a, op, b) to c
 }
 
-private fun String.parseGateBool() : Boolean {
+private fun String.parseGateBool(): Boolean {
     return when {
         this == "1" -> true
         this == "0" -> false
         else -> error("Invalid gate value: $this")
     }
+}
+
+private fun Rule.`only least significant z can be xor`(leastSignificant: String): Boolean =
+    second.startsWith("z") && first.second != "XOR" && second != leastSignificant
+
+private fun Rule.`xor only on intermediate gates `(): Boolean {
+    val (a, op, b) = first
+    val output = second
+    return op == "XOR" &&
+            output[0] !in listOf('x', 'y', 'z') &&
+            a[0] !in listOf('x', 'y', 'z') &&
+            b[0] !in listOf('x', 'y', 'z')
+}
+
+private fun Rule.`and not on x00 and output is not in a or`(rules: List<Rule>) : Set<String> {
+    val (a, op, b) = first
+    if (op == "AND" && a != "x00" && b != "x00") {
+        return rules.mapNotNull { (swapLeft, _) ->
+            val (swapA, swapOp, swapB) = swapLeft
+            if ((second == swapA || second == swapB) && swapOp != "OR") {
+                second
+            } else {
+                null
+            }
+        }.toSet()
+    }
+    return emptySet()
+}
+
+private fun Rule.`xor should swap, if there is a or with remaining operators`(rules: List<Rule>) : Set<String> {
+    if (first.second == "XOR") {
+        return rules.mapNotNull { otherRule ->
+            val (a, op, b) = otherRule.first
+            if ((second == a || second == b) && op == "OR") {
+                second
+            } else {
+                null
+            }
+        }.toSet()
+    }
+    return emptySet()
+}
+
+private fun List<Rule>.wrongWires(): Set<String> {
+    val result = mutableSetOf<String>()
+    val maxZ = filter { it.second.startsWith("z") }
+        .map { it.second }
+        .maxByOrNull { it.substring(1).toInt() } ?: error("No z wire found")
+    forEach { rule ->
+        if (rule.`only least significant z can be xor`(maxZ)) {
+            result.add(rule.second)
+        }
+
+        if (rule.`xor only on intermediate gates `()) {
+            result.add(rule.second)
+        }
+
+        result.addAll(rule.`and not on x00 and output is not in a or`(this))
+        result.addAll(rule.`xor should swap, if there is a or with remaining operators`(this))
+
+    }
+    return result
 }
 
 private typealias Gate = Pair<String, Boolean>
