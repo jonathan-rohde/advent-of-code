21b81de7bff434017caa9a91ee03aba735f8005c
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-25 06:37:17 +0100

    day25

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,20 +1,71 @@
 import utils.readInput
 import utils.testAndPrint
+import utils.toIntList
 
 fun main() {
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
-    }
+        val keyAndLocks = input.parse()
+        val keys = keyAndLocks.filter { !it.second }.map { it.first }
+        val locks = keyAndLocks.filter { it.second }.map { it.first }
 
-    fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        var result = 0L
+        keys.forEach { key ->
+            locks.forEach { lock ->
+                if (keyMatchesLock(key, lock)) {
+                    result++
+                }
+            }
+        }
+
+        return result
     }
 
     val testInput = readInput("Day25_test")
-    part1(testInput).testAndPrint(1)
-    part2(testInput).testAndPrint(1)
+    part1(testInput).testAndPrint(3L)
 
     val input = readInput("Day25")
     part1(input).testAndPrint()
-    part2(input).testAndPrint()
+}
+
+private fun List<String>.parse(): List<Pair<List<Int>, Boolean>> {
+    val result = mutableListOf<Pair<List<Int>, Boolean>>()
+
+    for (i in indices step 8) {
+        val lockKey = this.subList(i, i + 7).parseKey()
+        val isLock = this[i] == "#####"
+
+        result.add(lockKey to isLock)
+    }
+
+
+    return result
+}
+
+private fun List<String>.parseKey(): List<Int> {
+    return this.map {
+        val result = mutableListOf(0, 0, 0, 0, 0)
+        for (i in it.indices) {
+            if (it[i] == '#') {
+                result[i] = 1
+            }
+        }
+        result
+    }
+        .reduce { acc, ints ->
+            val result = mutableListOf(0, 0, 0, 0, 0)
+            for (i in acc.indices) {
+                result[i] = acc[i] + ints[i]
+            }
+            result
+        }
+        .map { it - 1 }
+}
+
+private fun keyMatchesLock(key: List<Int>, lock: List<Int>): Boolean {
+    key.forEachIndexed { index, i ->
+        if (i + lock[index] > 5) {
+            return false
+        }
+    }
+    return true
 }
