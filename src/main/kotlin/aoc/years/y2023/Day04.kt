f75ef139d4205a7096244e63a41f22af2563d4d1
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2024-12-26 22:12:44 +0100

    2023 day 4 part 2

--- src/main/kotlin/aoc/years/y2023/Day04.kt
+++ src/main/kotlin/aoc/years/y2023/Day04.kt
@@ -4,14 +4,26 @@ import aoc.common.Day
 import aoc.common.printResults
 import utils.toIntList
 
-class Day04 : Day(year = 2023, day = 4, test = 13 to null) {
+class Day04 : Day(year = 2023, day = 4, test = 13 to 30) {
     override fun part1(input: List<String>): Any {
         return input.parseCards()
-            .sumOf { it.getValue() }
+            .sumOf { it.getValue().first }
     }
 
     override fun part2(input: List<String>): Any {
-        return -1L
+        val cards = input.parseCards()
+        val interim = input.parseCards()
+            .map { it.getValue().second }.toMutableList()
+        val cardNumbers = cards.mapIndexed { index, _ -> index to 1 }.toMap(mutableMapOf())
+
+        interim.indices.forEach { index ->
+            val count = interim[index]
+            (1 .. count).forEach { i ->
+                cardNumbers[index + i] = cardNumbers[index + i]!! + (cardNumbers[index] ?: 0)
+            }
+        }
+
+        return cardNumbers.values.sum()
     }
 
 }
@@ -19,16 +31,19 @@ class Day04 : Day(year = 2023, day = 4, test = 13 to null) {
 fun main() {
     Day04().execute().printResults()
 }
+
 private fun List<String>.parseCards(): List<Pair<List<Int>, List<Int>>> {
     return map {
-        Pair(it.substring(it.indexOf(":") + 1, it.indexOf("|")).toIntList(),
-            it.substring( it.indexOf("|") + 1).toIntList())
+        Pair(
+            it.substring(it.indexOf(":") + 1, it.indexOf("|")).toIntList(),
+            it.substring(it.indexOf("|") + 1).toIntList()
+        )
     }
 }
 
-private fun Pair<List<Int>, List<Int>>.getValue(): Int {
+private fun Pair<List<Int>, List<Int>>.getValue(): Pair<Int, Int> {
     var result = 1
-    var found = false
+    var count = 0
     val left = ArrayDeque<Int>()
     left.addAll(first.sorted())
     val right = ArrayDeque<Int>()
@@ -40,7 +55,7 @@ private fun Pair<List<Int>, List<Int>>.getValue(): Int {
             result = result shl 1
             left.removeFirst()
             right.removeFirst()
-            found = true
+            count++
             continue
         }
         if (checkNumber < left[0]) {
@@ -52,5 +67,5 @@ private fun Pair<List<Int>, List<Int>>.getValue(): Int {
             continue
         }
     }
-    return result shr 1
+    return (result shr 1) to count
 }
