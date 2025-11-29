e0f0ecea725e8f9900036bd6e4d37f7b770ad416
Jonathan Rohde <johnny@parallel-bits.de>
2025-11-29 15:34:29 +0100

    refactor

--- src/main/kotlin/aoc/years/y2023/Day05.kt
+++ src/main/kotlin/aoc/years/y2023/Day05.kt
@@ -3,8 +3,6 @@ package aoc.years.y2023
 import aoc.common.Day
 import aoc.common.printResults
 import utils.toLongList
-import kotlin.math.max
-import kotlin.math.min
 
 class Day05 : Day(year = 2023, day = 5, test = 35L to 46L) {
     override fun part1(input: List<String>): Any {
@@ -13,7 +11,6 @@ class Day05 : Day(year = 2023, day = 5, test = 35L to 46L) {
     }
 
     override fun part2(input: List<String>): Any {
-//        return -1
         val seedMap = input.parseSeedMap(::parseSeeds2)
         return seedMap.findSmallesLocation()
     }
@@ -56,72 +53,25 @@ private fun SeedMap.maximum(): Long =
         .maxBy { it.endInclusive }
         .endInclusive
 
-private val cache = mutableMapOf<Long, Long>()
-
 private fun SeedMap.findSmallesLocation(): Long {
     val min = minimum()
     val max = maximum()
-//    val list = seeds.sortedBy { it.startInclusive }.map {
-    val list = (min..max).asSequence()
-//        generateSequence(it.startInclusive) { current ->
-//            (current + 1).takeIf { next -> next <= it.endInclusive }
-//        }
+    val foundSeed = (min..max).asSequence()
         .map { possibleLoc ->
-            possibleLoc.let { num -> determineSource(num, location) }
-            .let { num -> determineSource(num, humidiy) }
-            .let { num -> determineSource(num, temperature) }
-            .let { num -> determineSource(num, light) }
-            .let { num -> determineSource(num, water) }
-            .let { num -> determineSource(num, fertilizer) }
-            .let { num -> determineSource(num, soil) }
+            listOf(location, humidiy, temperature, light, water, fertilizer, soil)
+                .fold(possibleLoc) { num, map ->
+                    determineSource(num, map)
+                }
         }
 
         .first { seed ->
             seeds.any { it.startInclusive <= seed && it.endInclusive >= seed }
         }
-//    }
-    return listOf(list).map { s ->
-        determineTarget(s, soil)
-            .let { num -> determineTarget(num, fertilizer) }
-            .let { num -> determineTarget(num, water) }
-            .let { num -> determineTarget(num, light) }
-            .let { num -> determineTarget(num, temperature) }
-            .let { num -> determineTarget(num, humidiy) }
-            .let { num -> determineTarget(num, location) }
-    }.min()
-
-//    return seeds.asSequence()
-//        .map {
-//            generateSequence(it.startInclusive) { current ->
-//                (current + 1).takeIf { next -> next <= it.endInclusive }
-//            }
-//                .map { num -> determineTarget(num, soil) }
-//                .map { num -> determineTarget(num, fertilizer) }
-//                .map { num -> determineTarget(num, water) }
-//                .map { num -> determineTarget(num, light) }
-//                .map { num -> determineTarget(num, temperature) }
-//                .map { num -> determineTarget(num, humidiy) }
-//                .map { num -> determineTarget(num, location) }
-//                .min()
-//        }
-//        .min()
+    return listOf(soil, fertilizer, water, light, temperature, humidiy, location)
+        .fold(foundSeed) { num, map ->
+            determineTarget(num, map)
+        }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 private data class SeedMap(
@@ -136,6 +86,7 @@ private data class SeedMap(
 ) {
 
 }
+
 private fun List<String>.createIndexMap(): Map<String, Int> {
     val result = mutableMapOf<String, Int>()
     mapIndexed { i, r ->
@@ -145,14 +96,17 @@ private fun List<String>.createIndexMap(): Map<String, Int> {
     }
     return result
 }
+
 private fun parseSeeds(string: String): List<Range> {
     val values = string.substring("seeds: ".length).toLongList()
     return values.map { Range(it, it) }
 }
+
 private fun parseSeeds2(string: String): List<Range> {
     val values = string.substring("seeds: ".length).toLongList()
     return values.windowed(size = 2, step = 2).map { Range(it[0], it[0] + it[1] - 1) }
 }
+
 private fun List<String>.parseSeedMap(f: (String) -> List<Range>): SeedMap {
     val index = createIndexMap()
     return SeedMap(
@@ -170,7 +124,7 @@ private fun List<String>.parseSeedMap(f: (String) -> List<Range>): SeedMap {
 private data class Range(val startInclusive: Long, val endInclusive: Long) {
 
     fun contains(value: Long): Boolean {
-        return value in startInclusive .. endInclusive
+        return value in startInclusive..endInclusive
     }
 
     fun offset(value: Long): Int {
@@ -183,6 +137,7 @@ private data class Range(val startInclusive: Long, val endInclusive: Long) {
         }.first()
     }
 }
+
 private fun List<String>.extractParts(start: Int): List<Pair<Range, Range>> {
     val parts = mutableListOf<Pair<Range, Range>>()
     var index = start + 1
@@ -190,8 +145,8 @@ private fun List<String>.extractParts(start: Int): List<Pair<Range, Range>> {
         val (destinationStart, sourceStart, steps) = this[index].toLongList()
         parts.add(
             Range(sourceStart, sourceStart + steps - 1)
-            to
-            Range(destinationStart, destinationStart + steps - 1)
+                    to
+                    Range(destinationStart, destinationStart + steps - 1)
         )
         index++
     }
