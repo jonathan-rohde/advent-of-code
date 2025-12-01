b76186d04ce0575a5f5c1e0c74676c5f4ab4b2c4
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-01 09:51:44 +0100

    2025 day01: bit of refactoring

--- src/main/kotlin/aoc/years/y2025/Day01.kt
+++ src/main/kotlin/aoc/years/y2025/Day01.kt
@@ -2,7 +2,6 @@ package aoc.years.y2025
 
 import aoc.common.Day
 import aoc.common.printResults
-import kotlin.math.abs
 
 class Day01 : Day(year = 2025, day = 1, test = 3 to 6) {
     override fun part1(input: List<String>): Int {
@@ -23,21 +22,19 @@ private fun List<String>.turnDials(): Int {
     var total = 0
     var current = 50
     for (line in this) {
-        when (line[0]) {
-            'L' -> {
-                current -= line.substring(1).toInt()
-                current = (current + 100) % 100
-            }
-            'R' -> {
-                current += line.substring(1).toInt()
-                current %= 100
-            }
-        }
+        val clicks = line.substring(1).toInt() * line.directionToValue()
+        current = (current + clicks + 100) % 100
         if (current == 0) {
             total += 1
         }
     }
     return total
+}
+
+private fun String.directionToValue(): Int = when (this[0]) {
+    'L' -> -1
+    'R' -> 1
+    else -> throw IllegalArgumentException("Invalid direction")
 }
 
 private fun List<String>.turnDials2(): Int {
