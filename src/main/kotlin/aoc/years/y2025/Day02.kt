3ca8b4d4199f3946a8544d25555be5c14999f022
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-02 09:42:07 +0100

    2025 day02: refactor

--- src/main/kotlin/aoc/years/y2025/Day02.kt
+++ src/main/kotlin/aoc/years/y2025/Day02.kt
@@ -2,23 +2,19 @@ package aoc.years.y2025
 
 import aoc.common.Day
 import aoc.common.printResults
-import java.time.OffsetDateTime
-import java.time.ZoneId
 
 class Day02 : Day(year = 2025, day = 2, test = 1227775554L to 4174379265L) {
     override fun part1(input: List<String>): Long {
-        return input[0].parseRanges()
-            .sumOf {
-                (it.first..it.second).filter { id -> !id.isValidId()  }.sum()
-            }
+        return input[0].parseRanges().sumOf {
+            (it.first..it.second).filter { id -> !id.isValidId() }.sum()
+        }
     }
 
     override fun part2(input: List<String>): Long {
-        return input[0].parseRanges()
-            .sumOf {
-                println("Processing range: ${it.first} to ${it.second}")
-                (it.first..it.second).filter { id -> id.isPeriodicId()  }.sum()
-            }
+        return input[0].parseRanges().sumOf {
+            println("Processing range: ${it.first} to ${it.second}")
+            (it.first..it.second).filter { id -> id.isPeriodicId() }.sum()
+        }
     }
 }
 
@@ -28,38 +24,33 @@ fun main() {
 
 
 private fun String.parseRanges(): List<Pair<Long, Long>> {
-    return split(",")
-        .map { range ->
-            val (start, end) = range.split("-").map { it.toLong() }
-            start to end
-        }
+    return split(",").map { range ->
+        val (start, end) = range.split("-").map { it.toLong() }
+        start to end
+    }
 }
 
 private val idCache = mutableMapOf<Long, Boolean>()
 
 private fun Long.isValidId(): Boolean {
-    if (idCache.containsKey(this)) {
-        return idCache[this]!!
-    }
-    val str = toString()
-    if (str.length % 2 != 0) return true
-    val mid = str.length / 2
-    val (first, second) = str.take(mid) to str.substring(str.length - mid)
-    return (first != second).also { idCache[this] = it }
+    if (idCache.containsKey(this)) return idCache[this]!!
+
+    return with(toString()) {
+        if (length % 2 != 0) return true
+        val (first, second) = take(length / 2) to takeLast(length / 2) // to str.substring(str.length - mid)
+        (first != second)
+    }.also { idCache[this] = it }
 }
 
 private val patternCache = mutableMapOf<Long, Boolean>()
 
 private fun Long.isPeriodicId(): Boolean {
-    if (patternCache.containsKey(this)) {
-        return patternCache[this]!!
-    }
-    val str = toString()
-    return (1..str.length / 2).any { size ->
-        val pattern = str.take(size)
-        val repeated = pattern.repeat(str.length / size)
-        repeated == str
-    }.also {
-        patternCache[this] = it
-    }
+    if (patternCache.containsKey(this)) return patternCache[this]!!
+
+    return with(toString()) {
+        (1..length / 2).any { size ->
+            val repeated = this.take(size).repeat(length / size)
+            repeated == this
+        }
+    }.also { patternCache[this] = it }
 }
