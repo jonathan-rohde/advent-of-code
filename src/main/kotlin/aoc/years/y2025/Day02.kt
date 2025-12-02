91285c4c27c047ab4256c47b5b0f14287a16a9ba
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-02 08:36:27 +0100

    2025 day02

--- src/main/kotlin/aoc/years/y2025/Day02.kt
+++ src/main/kotlin/aoc/years/y2025/Day02.kt
@@ -2,20 +2,61 @@ package aoc.years.y2025
 
 import aoc.common.Day
 import aoc.common.printResults
-import utils.isSorted
-import utils.toIntList
-import kotlin.math.abs
 
-class Day02 : Day(year = 2025, day = 2, test = 11 to 31) {
-    override fun part1(input: List<String>): Int {
-        return 11
+class Day02 : Day(year = 2025, day = 2, test = 1227775554L to 4174379265L) {
+    override fun part1(input: List<String>): Long {
+        return input[0].parseRanges()
+            .sumOf {
+                (it.first..it.second).filter { id -> !id.isValidId()  }.sum()
+            }
     }
 
-    override fun part2(input: List<String>): Int {
-        return 31
+    override fun part2(input: List<String>): Long {
+        return input[0].parseRanges()
+            .sumOf {
+                println("Processing range: ${it.first} to ${it.second}")
+                (it.first..it.second).filter { id -> id.isPeriodicId()  }.sum()
+            }
     }
 }
 
 fun main() {
     Day02().execute().printResults()
+}
+
+
+private fun String.parseRanges(): List<Pair<Long, Long>> {
+    return split(",")
+        .map { range ->
+            val (start, end) = range.split("-").map { it.toLong() }
+            start to end
+        }
+}
+
+private fun Long.isValidId(): Boolean {
+    val str = toString()
+    if (str.length % 2 != 0) return true
+    val mid = str.length / 2
+    val (first, second) = str.take(mid) to str.substring(str.length - mid)
+    return (first != second).also {
+        if (!it) {
+            println("Invalid ID2: $this")
+        }
+    }
+}
+
+private val patternCache = mutableMapOf<Long, Boolean>()
+
+private fun Long.isPeriodicId(): Boolean {
+    if (patternCache.containsKey(this)) {
+        return patternCache[this]!!
+    }
+    val str = toString()
+    return (1..str.length / 2).any { size ->
+        val pattern = str.take(size)
+        val repeated = pattern.repeat(str.length / size)
+        repeated == str
+    }.also {
+        patternCache[this] = it
+    }
 }
