1c12416164b2fd46c2be47734a70cbd98d5d0a0d
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-03 09:39:54 +0100

    2025 day03

--- src/main/kotlin/aoc/years/y2025/Day03.kt
+++ src/main/kotlin/aoc/years/y2025/Day03.kt
@@ -3,16 +3,31 @@ package aoc.years.y2025
 import aoc.common.Day
 import aoc.common.printResults
 
-class Day03 : Day(year = 2025, day = 3, test = 11 to 31) {
-    override fun part1(input: List<String>): Int {
-        return 11
+class Day03 : Day(year = 2025, day = 3, test = 357L to null) {
+    override fun part1(input: List<String>): Long {
+        return input.sumOf {bank ->
+            bank.toBatteries().findMax(2).toLong()
+        }
     }
 
-    override fun part2(input: List<String>): Int {
-        return 31
+    override fun part2(input: List<String>): Long {
+        return input.sumOf { bank ->
+            bank.toBatteries().findMax(12).toLong()
+        }
     }
 }
 
 fun main() {
     Day03().execute().printResults()
+}
+
+private fun String.toBatteries(): List<Int> =
+    map { it.digitToInt() }
+
+private fun List<Int>.findMax(n: Int): String {
+    if (n == 0) return ""
+
+    val next = take(size - n + 1).max()
+    val remaining = drop(indexOf(next) + 1).findMax(n - 1)
+    return next.toString() + remaining
 }
