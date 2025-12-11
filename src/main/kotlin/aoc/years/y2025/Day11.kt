aab5106baff895ed3e349df78a1509e11ea9d2e9
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-11 11:44:25 +0100

    2025 day11: merge into single function

--- src/main/kotlin/aoc/years/y2025/Day11.kt
+++ src/main/kotlin/aoc/years/y2025/Day11.kt
@@ -5,11 +5,11 @@ import aoc.common.printResults
 
 class Day11 : Day(year = 2025, day = 11, test = 5L to 2L, testFile2 = "Day11_test2") {
     override fun part1(input: List<String>): Long {
-        return input.toGraph().countPaths("you", "out")
+        return input.toGraph().countPaths(from = "you", to = "out")
     }
 
     override fun part2(input: List<String>): Long {
-        return input.toGraph().countPathsWithDetour("svr", "out")
+        return input.toGraph().countPaths(from = "svr", to = "out", detours = setOf("dac", "fft"))
     }
 }
 
@@ -19,51 +19,25 @@ fun main() {
 
 private fun Map<String, List<String>>.countPaths(
     from: String,
-    to: String
-): Long {
-    return if (from == to) 1
-    else getValue(from).sumOf { neighbor -> countPaths(neighbor, to) }
-}
-
-private fun Map<String, List<String>>.countPathsWithDetour(
-    from: String,
     to: String,
-    cache: MutableMap<Triple<String, Boolean, Boolean>, Long> = mutableMapOf(),
-    foundDAC: Boolean = false,
-    foundFFT: Boolean = false,
-): Long {
-    return cache.getOrPut(Triple(from, foundDAC, foundFFT)) {
-        if (from == to) {
-            return@getOrPut if (from.hasAllDetours(foundDAC, foundFFT)) 1 else 0
-        }
+    cache: MutableMap<Pair<String, Set<String>>, Long> = mutableMapOf(),
+    detours: Set<String> = emptySet(),
+    visits: Set<String> = emptySet()
+): Long = cache.getOrPut(Pair(from, visits)) {
+    if (from == to) {
+        if (visits.containsAll(detours)) 1 else 0
+    } else {
         getValue(from).sumOf { neighbor ->
-            countPathsWithDetour(
-                from = neighbor,
-                to = to,
-                cache = cache,
-                foundDAC = neighbor.hasDetour("dac", foundDAC),
-                foundFFT = neighbor.hasDetour("fft", foundFFT)
-            )
+            val newVisits = if (from in detours) visits + from else visits
+            countPaths(neighbor, to, cache, detours, newVisits)
         }
     }
-
-}
-
-private fun String.hasDetour(detour: String, rec: Boolean): Boolean {
-    return rec || this == detour
-}
-
-private fun String.hasAllDetours(
-    foundDAC: Boolean,
-    foundFFT: Boolean
-): Boolean {
-    return (foundDAC || this == "dac") && (foundFFT || this == "fft")
 }
 
 private fun List<String>.toGraph(): Map<String, List<String>> {
-    return associate {
-        val (from, to) = it.split(": ")
-        val neighbors = to.split(" ")
-        from to neighbors
+    return associate { line ->
+        line.split(": ").let { (from, to) ->
+            from to to.split(" ")
+        }
     }
 }
