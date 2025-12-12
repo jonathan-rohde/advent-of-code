b9a295ae81b5f97428265abe812b23645e63d4e4
Jonathan Rohde <jonathan.rohde@iits-consulting.de>
2025-12-12 15:01:18 +0100

    2025 day12

--- src/main/kotlin/aoc/years/y2025/Day12.kt
+++ src/main/kotlin/aoc/years/y2025/Day12.kt
@@ -2,17 +2,26 @@ package aoc.years.y2025
 
 import aoc.common.Day
 import aoc.common.printResults
+import utils.toIntList
 
-class Day12 : Day(year = 2025, day = 12, test = 11 to 31) {
-    override fun part1(input: List<String>): Int {
-        return 11
-    }
-
-    override fun part2(input: List<String>): Int {
-        return 31
-    }
+class Day12 : Day(year = 2025, day = 12, test = 3 to null) {
+    override fun part1(input: List<String>) = input.parseGrids().countValidAreas()
 }
 
 fun main() {
     Day12().execute().printResults()
 }
+
+private val Pair<Int, Int>.area get() = this.first * this.second
+
+private val List<Int>.shapeArea get() = sumOf { it * 9 }
+
+private fun List<Pair<Pair<Int, Int>, List<Int>>>.countValidAreas() =
+    count { (dim, shape) -> dim.area >= shape.shapeArea }
+
+private fun List<String>.parseGrids() = takeLastWhile { it.isNotBlank() }
+    .map { line ->
+        val (dimensions, shapes) = line.split(":")
+        val (cols, rows) = dimensions.split("x").map { it.toInt() }
+        Pair(cols, rows) to shapes.toIntList()
+    }
