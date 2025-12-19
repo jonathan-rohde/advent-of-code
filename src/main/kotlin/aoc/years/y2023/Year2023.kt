bc7fcc1c0e6fc18294d3e9c300ac76fc5b2d07da
Jonathan Rohde <johnny@parallel-bits.de>
2025-12-19 21:22:38 +0100

    2023: add days

--- src/main/kotlin/aoc/years/y2023/Year2023.kt
+++ src/main/kotlin/aoc/years/y2023/Year2023.kt
@@ -16,6 +16,14 @@ class Year2023 : Year {
                 Day06(),
                 Day07(),
                 Day08(),
+                Day09(),
+                Day10(),
+                Day11(),
+                Day12(),
+                Day13(),
+                Day14(),
+                Day15(),
+                Day16()
             ),
             year = 2024,
             opaqueResults = opaqueResults
