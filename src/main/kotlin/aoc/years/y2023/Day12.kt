04edea61a5d333e518e861895924e38fd18e97f8
Jonathan Rohde <johnny@parallel-bits.de>
2025-12-20 20:42:13 +0100

    2023 day12: part 2

--- src/main/kotlin/aoc/years/y2023/Day12.kt
+++ src/main/kotlin/aoc/years/y2023/Day12.kt
@@ -4,6 +4,7 @@ import aoc.common.Day
 import aoc.common.Part
 import aoc.common.printResults
 import utils.toIntList
+import kotlin.collections.mutableMapOf
 
 private val testInput = """
     ???.### 1,1,3
@@ -21,34 +22,71 @@ class Day12 : Day(
     part2 = Part(test = 525152L, testInput = testInput),
 ) {
     override fun part1(input: List<String>): Any {
-        return input.sumOf {
-            val (placeholders, numbers) = it.parse()
-            findValidPattern(placeholders, numbers)
+        return input.sumOf { line ->
+            line.parse().let { (placeholders, numbers) ->
+                placeholders.findValidPattern(numbers)
+            }
         }
     }
 
+    override fun part2(input: List<String>): Any {
+        return input.sumOf { line ->
+            line.parse().let { (placeholders, numbers) ->
+                (0..4).joinToString("?") { placeholders } to (0..4).flatMap { numbers}
+            }.let { (placeholders, numbers) ->
+                placeholders.findValidPattern(numbers)
+            }
+        }
+    }
 }
 
 fun main() {
     Day12().execute().printResults()
 }
 
-private fun findValidPattern(pattern: String, numbers: List<Int>): Long {
-    if (Pair(pattern, numbers).isValid()) return 1L
-    if (!pattern.contains('?')) return 0L
+private fun String.findValidPattern(
+    numbers: List<Int>,
+    cache: MutableMap<Pair<String, List<Int>>, Long> = mutableMapOf()
+): Long {
+    return cache.getOrPut(this to numbers) {
 
-    val nextIndex = pattern.indexOf('?')
-    val resultWithDot = findValidPattern(pattern.replaceRange(nextIndex, nextIndex + 1, "."), numbers)
-    val resultWithHash = findValidPattern(pattern.replaceRange(nextIndex, nextIndex + 1, "#"), numbers)
-    return resultWithDot + resultWithHash
+        if (none { it == '?' } && split(".").map { it.length }.filter { it > 0 } == numbers)
+            return@getOrPut 1L
 
-}
+        if (this.startsWith(".")) {
+            return@getOrPut this.drop(1).findValidPattern(numbers, cache)
+        }
+        if (this.startsWith("?")) {
+            val resultWithDot = this.drop(1).findValidPattern(numbers, cache)
+            val resultWithHash = "#${this.drop(1)}".findValidPattern(numbers, cache)
+            return@getOrPut resultWithDot + resultWithHash
+        }
+        if (this.startsWith("#")) {
+            if (numbers.isEmpty()) return@getOrPut 0L
+            val required = numbers.first()
+            val block = this.take(required)
+            val next = this.drop(required).take(1)
 
-private fun Pair<String, List<Int>>.isValid(): Boolean {
-    val (pattern, numbers) = this
-    if (pattern.any { it == '?' }) return false
-    val chunks = pattern.split(".").map { it.length }.filter { it > 0 }
-    return numbers == chunks
+            if (block.all { it == '#' }) {
+                if (next == "." || next == "?") {
+                    // found number
+                    return@getOrPut this.drop(required + 1).findValidPattern(numbers.drop(1), cache)
+                } else {
+                    // next is a # and invalid
+                    return@getOrPut 0L
+                }
+            } else if (block.contains("?")) {
+                // still possible
+                val replacement = replaceFirst('?', '#')
+                return@getOrPut replacement.findValidPattern(numbers, cache)
+            } else {
+                // block contains . and thus invalid
+                return@getOrPut 0L
+            }
+        }
+
+        return@getOrPut 0L
+    }
 }
 
 private fun String.parse(): Pair<String, List<Int>> {
