30a197190ad424bca75aaf1c77b2ce49a2d04f38
Jonathan Rohde <johnny@parallel-bits.de>
2025-12-24 14:28:16 +0100

    2015 day14

---
+++ src/main/kotlin/aoc/years/y2015/Day14.kt
@@ -0,0 +1,58 @@
+package aoc.years.y2015
+
+import aoc.common.Day
+import aoc.common.Part
+import aoc.common.printResults
+
+fun main() {
+    Day14().execute().printResults()
+}
+
+private val testInput = """
+    Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+    Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+""".trimIndent()
+
+class Day14 : Day(
+    year = 2015,
+    day = 14,
+    part1 = Part(test = 1120, testInput = testInput),
+    part2 = Part(test = 286, testInput = testInput),
+) {
+
+    override fun part1(input: List<String>): Any {
+        return input.map { it.toReindeer() }.maxOf { it.distanceAfter(2503) }
+    }
+
+    override fun part2(input: List<String>): Any {
+        val reindeers = input.map { it.toReindeer() }
+        val scores = reindeers.associateWith { 0 }.toMutableMap()
+        (1..2503).forEach { i ->
+            val standing = reindeers.map { it to it.distanceAfter(i) }.sortedByDescending { it.second }
+            val winningScore = standing.first().second
+            val winners = standing.takeWhile { it.second == winningScore }
+            winners.forEach { winner ->
+                scores[winner.first] = scores[winner.first]!! + 1
+            }
+        }
+
+        return scores.maxOf { it.value }
+    }
+}
+
+private data class Reindeer(val name: String, val speed: Int, val duration: Int, val pause: Int) {
+    fun distanceAfter(seconds: Int): Int {
+        val segment = duration + pause
+        val distancePerSegment = duration * speed
+        val amountSegments = seconds / segment
+        val remain = seconds % segment
+        val portion = if (duration <= remain) duration * speed else remain * speed
+        return amountSegments * distancePerSegment + portion
+    }
+}
+
+private val reindeerRegex = "([a-zA-Z]+) can fly ([0-9]+) km/s for ([0-9]+) seconds, but then must rest for ([0-9]+) seconds".toRegex()
+private fun String.toReindeer(): Reindeer {
+    val (name, speed, duration, pause) = reindeerRegex.find(this)!!.destructured
+    return Reindeer(name, speed.toInt(), duration.toInt(), pause.toInt())
+}
